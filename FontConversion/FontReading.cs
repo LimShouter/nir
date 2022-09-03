4b85f7bcc8fc41c5c0f0c22c9d1e650f768d959c
Vladislav <limshouter@gmail.com>
2022-09-03 06:52:54 +0300

    -added FontVision, FontReading

---
+++ FontConversion/FontReading.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+
+namespace FontConversion;
+
+[SuppressMessage("Interoperability", "CA1416:Проверка совместимости платформы")]
+public class FontReading
+{
+    private readonly PrivateFontCollection _privateFontCollection = new PrivateFontCollection();
+
+    public FontFamily GetFontFamilyByName(string name)
+    {
+        return _privateFontCollection.Families.FirstOrDefault(x => x.Name == name,FontFamily.GenericSerif);
+    }
+
+    public void AddFont(string fullFileName)
+    {
+        AddFont(File.ReadAllBytes(fullFileName));
+    }
+
+    private void AddFont(byte[] fontBytes)
+    {
+        var handle = GCHandle.Alloc(fontBytes, GCHandleType.Pinned);
+        IntPtr pointer = handle.AddrOfPinnedObject();
+        try
+        {
+            _privateFontCollection.AddMemoryFont(pointer, fontBytes.Length);
+        }
+        finally
+        {
+            handle.Free();
+        }
+    }
+}
