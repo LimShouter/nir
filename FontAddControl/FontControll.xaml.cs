597ec085eafef14e3e01fbe49af812d89fac0be0
Vladislav Krikunov <limshouter@gmail.com>
2022-09-06 11:32:11 +0300

    -added FontControl

---
+++ FontAddControl/FontControll.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace FontAddControl;
+
+public partial class FontControll : UserControl
+{
+    public FontControll()
+    {
+        InitializeComponent();
+    }
+}
